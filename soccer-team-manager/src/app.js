7236df8e7493df2e1a101d82285af4ba9e901584
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-02-02 15:25:44 -0300

    MIDDLEWARE

--- soccer-team-manager/src/app.js
+++ soccer-team-manager/src/app.js
@@ -4,52 +4,72 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+const validateTeam = require('./middlewares/validateTeam');
+const existingId = require('./src/middlewares/existingId');
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
 
 app.use(express.json());
-const teams = [
-    {
-      id: 1,
-      name: 'Grêmio Foot-Ball Porto Alegrense',
-      initials: 'GFPA',
-    },
-    {
-      id: 2,
-      name: 'Clube Atlético Mineiro',
-      initials: 'CAM',
-    },
-  ];
 
-  app.get('/teams', (req, res) => res.status(200).json({ teams }));
+app.get('/teams', (req, res) => res.json(teams));
 
-  app.post('/teams', (req, res) => {
-    const newTeam = { ...req.body };
-    teams.push(newTeam);
-  
-    res.status(201).json({ team: newTeam });
-  });
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
 
-  app.put('/teams/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, initials } = req.body;
-  
-    const updateTeam = teams.find((team) => team.id === Number(id));
-  
-    if (!updateTeam) {
-      res.status(404).json({ message: 'Team not found' });
-    }
-  
-    updateTeam.name = name;
-    updateTeam.initials = initials;
-    res.status(200).json({ updateTeam });
-  });
+app.post('/teams', validateTeam, (req, res) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    const team = { id: nextId, ...req.body };
+    teams.push(team);
+    nextId += 1;
+    res.status(201).json(team);
+  } else {
+    res.sendStatus(400);
+  }
+});
 
-  app.delete('/teams/:id', (req, res) => {
-    const { id } = req.params;
-    const arrayPosition = teams.findIndex((team) => team.id === Number(id));
-    teams.splice(arrayPosition, 1);
-  
-    res.status(200).end();
-  });
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+   // não precisamos mais conferir, com certeza o team existe
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
+});
+
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+  res.sendStatus(204);
+});
 
 module.exports = app;
+
+
+// Repare como o método POST foi definido de uma maneira mais direta e reaproveitável, deixando os detalhes
+//  de validação para o middleware validateTeam. Se a requisição chegou no segundo middleware, ele pode criar
+//   o time sem se preocupar com mais nada.
+
+// Os middlewares seguintes podem receber o next como um terceiro parâmetro, mas geralmente o último 
+// middleware de uma rota precisa responder a requisição. Portanto, se ele não for chamar outro middleware,
+//  não há necessidade de usar o objeto next e você pode escrever a função recebendo apenas dois parâmetros: os objetos req e res
+
+
+// E o que o middleware validateTeam faz?
+// 1️⃣ Faz uma validação básica que apenas confere se todas as propriedades esperadas estão presentes no req.body.
+
+// 2️⃣ Se a validação aprovar, esse middleware endereça a requisição para o próximo middleware, que efetivamente cria o time.
+
+// 3️⃣ Se a validação falhar, esse middleware retorna uma resposta com status 400 e nunca chama o próximo middleware. 400 é o 
+// código HTTP para Bad Request, indicando que existe algo errado na requisição.
